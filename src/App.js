29165dc30cd90ae231a775b3624a933e3b69c208
Srijan Sharma <srijans38@gmail.com>
2020-05-07 00:44:19 +0530

    animations

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hi!</h1>
+        <h1 className="hi">Hi!</h1>
         <p className="name">I am Srijan Sharma</p>
-        <p>
+        <p className="p1">
           I study Information Technology at Bhilai Institute of Technology,
           Durg. I have experience with Django, Flutter, Vue.js and React. You
           can find my work on <a href="https://github.com/srijans38">Github</a>.
         </p>
-        <p>
+        <p className="p2">
           Apart from this, I am a space nerd{" "}
           <span role="img" aria-label="space">
             🚀
@@ -26,7 +26,7 @@ function App() {
             🔵🔴
           </span>{" "}
         </p>
-        <p>
+        <p className="p3">
           You can also find me on{" "}
           <a href="https://www.instagram.com/theguywithabignose/">Instagram</a>{" "}
           or <a href="mailto:srijans38@gmail.com">Email</a>.
